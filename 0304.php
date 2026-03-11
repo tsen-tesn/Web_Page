e3869d650e88cc8daf4fcc46862cd2b2e98e9bdd
cyc <maggiet715dr@gmail.com>
2026-03-11 16:37:21 +0800

    update

---
+++ 0304.php
@@ -0,0 +1,5 @@
+<?php
+    $color = "#4b33ff";
+    $text = "113820031 陳彥岑";
+    echo "<span style='color: " . $color . "'>" . $text . "</span>";
+?>
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Web_Page
 web page
+
+***
+
+* php 檔案執行方法
+
+```
+http://localhost/你的檔案.php
+```
+
+## php 常用語法
